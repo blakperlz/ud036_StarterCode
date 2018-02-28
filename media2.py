570c62d0d784677dae0c32e04ee7a29d04420bf3
blakperlz <36514136+blakperlz@users.noreply.github.com>
2018-02-28 15:50:04 -0500

    Feedback 1 revisions
    Removed 'self' from the arguments.
    Changed commenting section.  was told to do one or other so I went with #.

--- media2.py
+++ media2.py
@@ -1,9 +1,11 @@
 import webbrowser
 import fresh_tomatoes
 
-#This is the parent class in case we wanted to add a new sction like TV shows or something
+#This is the parent class in case we wanted to add a new section
+#like TV shows or something.
+#class Video() is the parent class
+#class Movie() is the child class which inherits
 class Video():
-    """This is the inhereted variables for all classes"""
     def __init__(self, length, rating):
         print ("Duration and Rating Score Constructor")
         self.length = length
@@ -12,7 +14,6 @@ class Video():
 VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 #Added 'Video' as the partent so 'Movie' could inheret.
 class Movie(Video):
-    """This class provides a way to store movie related information"""
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, length, rating):
@@ -23,10 +24,10 @@ class Movie(Video):
         self.trailer_youtube_url = trailer_youtube
         Video.__init__(self, length, rating)
         
-    def show_trailer(self):
+    def show_trailer():
         webbrowser.open(self.trailer_youtube_url)
         
-    def show_poster(self):
+    def show_poster():
         webbrowser.open(self.poster_image_url)
 
 
