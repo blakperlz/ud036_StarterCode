1c43987a454687da59c9fc784b8f229d41b796b2
blakperlz <36514136+blakperlz@users.noreply.github.com>
2018-03-01 12:32:50 -0500

    Updates from Feedback2
    Using online pep editor made changes to breaking up the lines that were too long, spacing of sections, etc...  Change was made to entertainment_center2.py and media2.py file.  Confirmed that the code worked after.

--- media2.py
+++ media2.py
@@ -5,27 +5,35 @@ import fresh_tomatoes
 # like TV shows or something.
 # class Video() is the parent class
 # class Movie() is the child class which inherits
+
+
 class Video():
     def __init__(self, length, rating):
         print ("Duration and Rating Score Constructor")
         self.length = length
         self.rating = rating
 
+
 VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 # Added 'Video' as the partent so 'Movie' could inheret.
-class Movie(Video):  
+
+
+class Movie(Video):
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, length, rating):
-        print("Chiled contstructor movie")
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
-        Video.__init__(self, length, rating)
+
+    def __init__(
+            self, movie_title, movie_storyline,
+            poster_image, trailer_youtube, length,
+            rating):
+            print("Chiled contstructor movie")
+            self.title = movie_title
+            self.storyline = movie_storyline
+            self.poster_image_url = poster_image
+            self.trailer_youtube_url = trailer_youtube
+            Video.__init__(self, length, rating)
 
     def show_trailer():
         webbrowser.open(self.trailer_youtube_url)
-        
+
     def show_poster():
         webbrowser.open(self.poster_image_url)
-        
